9b2fb24952f7635e1950a5e4e0398bf7053080c6
LAI <lukalai1@orange.fr>
2023-11-27 16:33:58 +0100

    Avant correction

---
+++ Liasse/ConstructeurLiasseVehicule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liasse
+{
+    internal class ConstructeurLiasseVehicule
+    {
+        public void ConstruitBondeCommande(int idvehicule)
+        {
+
+        }
+
+        public void ConstruitDemandeImatriculation(int idvehicule)
+        {
+
+        }
+
+        public void Resultat()
+        {
+        }
+    }
+}
