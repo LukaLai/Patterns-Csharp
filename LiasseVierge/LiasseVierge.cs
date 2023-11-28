f098f3688dfb147470a57428519ce8e8616ae5dd
LAI <lukalai1@orange.fr>
2023-11-28 11:46:43 +0100

    Adapteur

---
+++ LiasseVierge/LiasseVierge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiasseVierge
+{
+    internal class LiasseVierge
+    {
+        public void ajoute(string document)
+        {
+
+        }
+        public void retire(string document)
+        {
+
+        }
+    }
+}
